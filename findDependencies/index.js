15c3d9220988a02693360b78bac7c3fa6bb3c3ba
Ray Lei <raylei@ISTWLAPAPPLE1811.local>
2019-01-18 16:49:59 +0800

    Get the usage list of each level2 modules that in project dependencies.

--- findDependencies/index.js
+++ findDependencies/index.js
@@ -1,33 +1,33 @@
-const { lstatSync, readdirSync, existsSync, writeFile } = require('fs');
+const fs = require('fs');
 const path = require('path');
 const packageJson = require('../package');
 const dependencies = Object.keys(packageJson.dependencies);
 
 // const dependencyKeys = Object.keys(dependencies);
-const isDirectory = source => lstatSync(source).isDirectory()
+const isDirectory = source => fs.lstatSync(source).isDirectory()
 const getDirectories = source =>
-  readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+  fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
 const getModuleName = modulePath => modulePath.split('/').pop();
 
-// const innerPackages = getDirectories('../node_modules');
-// console.log('innerPackages', Array.isArray(innerPackages));
-
-const nestedModules = dependencies.reduce((pV, cV) => {
-  const nestedModulesPath = path.resolve(`../node_modules/${cV}/node_modules`);
-  if (existsSync(nestedModulesPath)) {
+const level2ModuleUsageList = {};
+dependencies.map(dependency => {
+  const nestedModulesPath = path.resolve(`../node_modules/${dependency}/node_modules`);
+  if (fs.existsSync(nestedModulesPath)) {
     const nestedPackageNames = getDirectories(nestedModulesPath)
       .filter(path => !path.includes('.bin'))
-      .map(path => getModuleName(path));
+      .map(path => {
+        const moduleName = getModuleName(path);
+        const version = require(`${path}/package.json`).version;
+        return `${moduleName}-${version}`;
+      });
 
-    return {
-      ...pV,
-      [cV]: pV[cV] ? [...pV[cV], ...nestedPackageNames] : [...nestedPackageNames],
-    }
+    nestedPackageNames.forEach(nestedPackageName => {
+      if (!level2ModuleUsageList[nestedPackageName]) level2ModuleUsageList[nestedPackageName] = [dependency];
+      else level2ModuleUsageList[nestedPackageName].push(dependency);
+    })
   }
-
-  return pV;
-}, {});
-console.log('------- Nested Modules -------', nestedModules);
+});
+console.log('------- Level 2 Modules Usage List -------', level2ModuleUsageList);
 
 
 // const appendData = (targetPath, data) => {
